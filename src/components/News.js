5281d3cba6e9f52f272e4df0150ab6bf38149acb
Ahmad1630 <ahmadwaqar284@gmail.com>
2025-12-31 16:55:22 +0500

    added News.js
    news section file created

---
+++ src/components/News.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class News extends Component {
+  static propTypes = {
+
+  }
+
+  render() {
+    return (
+      <div>
+        This is the news section
+      </div>
+    )
+  }
+}
+
+export default News
