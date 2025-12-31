77b6fecfcc5bccbdd66831947492fbe5b37f16dd
Ahmad1630 <ahmadwaqar284@gmail.com>
2025-12-31 15:55:35 +0500

    maintenance and adding navbar
    added navbar using rcep boilerplate instead of rfc and did some other changes like adding bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import './App.css';
 import React, { Component } from 'react'
 
 export default class App extends Component {
+  c = 'John'; // This is a class variable
   render() {
     return (
-      <div>
-        
-      </div>
+      <div>Hello my first class based component {this.c}</div>
     )
   }
 }
