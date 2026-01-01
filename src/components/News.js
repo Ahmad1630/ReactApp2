43a57218dae53def3200dc466970ed56f26f4918
Ahmad1630 <ahmadwaqar284@gmail.com>
2026-01-01 14:26:37 +0500

    Updated News and NewsItem
    added example news cards and passed them from newsitem to news by using props

--- src/components/News.js
+++ src/components/News.js
@@ -1,26 +1,37 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import NewsItem from './NewsItem.js'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import NewsItem from "./NewsItem.js";
 
 export class News extends Component {
-  static propTypes = {
-
-  }
+  static propTypes = {};
 
   render() {
     return (
-      <div>
-        This is the news section
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+      <div className="container my-5">
+
+        <h1 className="mb-3">News</h1>
+
+
+        <div className="row">
+
+            <div className="col-md-4">
+              <NewsItem title="My title" description="This is a description" imgurl="https://www.aljazeera.com/wp-content/uploads/2025/12/AP25365153696099-1767157848.jpg?resize=1920%2C1367"/>
+            </div>
+
+            <div className="col-md-4">
+              <NewsItem title="My title" description="This is a description" />
+            </div>
+
+            <div className="col-md-4">
+              <NewsItem title="My title" description="This is a description" />
+            </div>
+
+        </div>
+
 
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,10 +7,22 @@ export class NewsItem extends Component {
   }
 
   render() {
+   let  {title, description, imgurl} = this.props;
     return (
-      <div>
-        This is a news item
+
+      <div> 
+
+        <div className="card" style={{width: "18rem"}}>
+          <img src={imgurl} className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{description}</p>
+              <a href="#" className="btn btn-primary">Read more</a>
+            </div>
+        </div>     
+
       </div>
+
     )
   }
 }
